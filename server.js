2fe4a956b761afd2848384e73f61b6e6f6057c11
adriano-bertanha <adriano.bertanha.pb@compasso.com.br>
2025-04-18 09:45:47 -0300

    build:Finished server config for dev enviromen.[Trello #EvkirrEJ]

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 /* server.js */
-    
-const cors = require('cors');
-const next = require('next');
-const Pusher = require('pusher');
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
-const Sentiment = require('sentiment');
 
-const dev = process.env.NODE_ENV !== 'production';
-const port = process.env.PORT || 3000;
+const express = require("express");
+const next = require("next");
+const cors = require("cors");
+const Pusher = require("pusher");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+const Sentiment = require("sentiment");
 
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handler = app.getRequestHandler();
 const sentiment = new Sentiment();
@@ -22,29 +20,38 @@ const pusher = new Pusher({
   key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_APP_SECRET,
   cluster: process.env.PUSHER_APP_CLUSTER,
-  encrypted: true
+  useTLS: true,
 });
 
-app.prepare()
-  .then(() => {
+app
+.prepare()
+.then(() => {
+  const server = express();
   
-    const server = express();
-    
-    server.use(cors());
-    server.use(bodyParser.json());
-    server.use(bodyParser.urlencoded({ extended: true }));
-    
-    server.get('*', (req, res) => {
-      return handler(req, res);
-    });
-    
-    server.listen(port, err => {
-      if (err) throw err;
-      console.log(`> Ready on http://localhost:${port}`);
-    });
-    
+  // Middleware
+  server.use(cors());
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({ extended: true }));
+  
+  // Next.js request handling
+  server.get("*", (req, res) => {
+    return handler(req, res);
+  });
+  
+  // error handling
+  server.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+  });
+  
+  const port = process.env.PORT || 3000;
+  
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
   })
-  .catch(ex => {
+  .catch((ex) => {
     console.error(ex.stack);
     process.exit(1);
   });
